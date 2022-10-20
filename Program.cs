1fd30d768968443eaaec5e35870fc0cab56e5652
Your Name <you@example.com>
2022-10-20 17:38:36 +0300

    Add Readme

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace Karakter_Tersten_Yazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz:");
+            string cumle = Console.ReadLine();
+            string yeniCumle = "";
+            string harfTutucu = "";
+            for (int i = 0; i <= cumle.Length; i++)
+            {
+                if (i == 0)
+                {
+                    harfTutucu = cumle[i].ToString();
+                }
+                else if (i == cumle.Length)
+                {
+                    yeniCumle += harfTutucu;
+                }
+                else if (cumle[i-1].ToString() == " ")
+                {
+                    continue;
+                }
+                else if (cumle[i].ToString() == " ")
+                {
+                    yeniCumle += harfTutucu + " ";
+                    harfTutucu = cumle[i+1].ToString();
+                }
+                else
+                {
+                    yeniCumle += cumle[i].ToString();
+                }
+            }
+            Console.WriteLine(yeniCumle);
+        }
+    }
+}
